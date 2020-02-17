55818862cb0e9afcb41db6055068499fc0a383b0
walter.shila <SAMpam1988>
2020-02-17 15:05:19 +0200

    Added headless mode running readme note

--- src/test/java/takeAway/ParallelExecution.java
+++ src/test/java/takeAway/ParallelExecution.java
@@ -1,15 +1,15 @@
 package takeAway;
 
-import org.openqa.selenium.JavascriptExecutor;
+import org.junit.Assert;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
 
 public class ParallelExecution extends BaseUtilities {
 
-    private OrderMealSteps orderMealSteps = new OrderMealSteps();
     private String propertyFile = "./src/test/resources/takeaway.properties";
     private String appURL = getConfigPropertyValue(propertyFile, "url");
 
@@ -23,49 +23,42 @@ public class ParallelExecution extends BaseUtilities {
         String windowsDriverLocation = getConfigPropertyValue(propertyFile, "windows_driver_location");
         String runInHeadlessMode = getConfigPropertyValue(propertyFile, "headless");
 
-        switch (browser) {
-            case "chrome":
-                /**
-                 * Driver setup for google chrome web browser
-                 */
-                String chromeDriverPath = null;
+       switch(browser) {
+               /**
+                * Driver setup for google chrome web browser
+                */
+           case "chrome" :
+               String chromeDriverPath = null;
 
-                if (this.getOsName().equalsIgnoreCase("Windows")) {
-                    chromeDriverPath = macDriverLocation + getConfigPropertyValue(propertyFile, "windows_driver_chrome");
-                } else if (this.getOsName().equalsIgnoreCase("Mac OS")) {
-                    chromeDriverPath = macDriverLocation + getConfigPropertyValue(propertyFile, "mac_driver_chrome");
-                } else if (this.getOsName().equalsIgnoreCase("Linux")) {
-                    chromeDriverPath = macDriverLocation + getConfigPropertyValue(propertyFile, "linux_driver_chrome");
-                }
-                logger.info("This is the chrome driver path is :::: " + chromeDriverPath);
+               if (this.getOsName().equalsIgnoreCase("Windows")) {
+                   chromeDriverPath = macDriverLocation + getConfigPropertyValue(propertyFile, "windows_driver_chrome");
+               } else if (this.getOsName().equalsIgnoreCase("Mac OS")) {
+                   chromeDriverPath = macDriverLocation + getConfigPropertyValue(propertyFile, "mac_driver_chrome");
+               } else if (this.getOsName().equalsIgnoreCase("Linux")) {
+                   chromeDriverPath = macDriverLocation + getConfigPropertyValue(propertyFile, "linux_driver_chrome");
+               }
+               logger.info("This is the chrome driver path is :::: " + chromeDriverPath);
 
-                String absoluteChromeDriverPath = toAbsolutePath(chromeDriverPath);
-                logger.info("This is the chrome driver real path is :::: " + absoluteChromeDriverPath);
+               String absoluteChromeDriverPath = toAbsolutePath(chromeDriverPath);
+               logger.info("This is the chrome driver real path is :::: " + absoluteChromeDriverPath);
 
-                System.setProperty("webdriver.chrome.driver", absoluteChromeDriverPath);
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("test-type");
-                options.addArguments("window-size=1980,1080");
-                options.addArguments("--disable-extensions");
-                options.addArguments("--proxy-server='direct://'");
-                options.addArguments("--proxy-bypass-list=*");
+               System.setProperty("webdriver.chrome.driver", absoluteChromeDriverPath);
+               ChromeOptions options = new ChromeOptions();
+               options.addArguments("test-type");
+               options.addArguments("window-size=1980,1080");
+               options.addArguments("--disable-extensions");
+               options.addArguments("--proxy-server='direct://'");
+               options.addArguments("--proxy-bypass-list=*");
 
-                if (runInHeadlessMode.equalsIgnoreCase("yes")) {
-                    options.addArguments("--headless");
-                }
-                driver = new ChromeDriver(options);
+               if (runInHeadlessMode.equalsIgnoreCase("yes")) {
+                   options.addArguments("--headless");
+               }
+               driver = new ChromeDriver(options);
+               driver.manage().window().maximize();
+               driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+               break;
 
-                try {
-                    jExecutor = (JavascriptExecutor) driver;
-                    driver.manage().window().maximize();
-                    driverWait = new WebDriverWait(driver, 10);
-                } catch (Exception ex) {
-                    logger.info("The stack trace here happens when I try to maximize the screen");
-                    logger.info(ex.getMessage());
-                }
-                break;
-
-            case "firefox":
+           case "firefox":
 
                 /**
                  * Driver setup for firefox web browser
@@ -87,20 +80,13 @@ public class ParallelExecution extends BaseUtilities {
 
                 System.setProperty("webdriver.gecko.driver", absoluteFirefoxDriverPath);
                 driver = new FirefoxDriver();
-
-                try {
-
-                    jExecutor = (JavascriptExecutor) driver;
-                    driver.manage().window().maximize();
-                    driverWait = new WebDriverWait(driver, 10);
-                } catch (Exception ex) {
-                    logger.info("The stack trace here happens when I try to maximize the screen");
-                    logger.info(ex.getMessage());
-                }
+                driver.manage().window().maximize();
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                break;
         }
     }
 
-    @AfterClass
+    @AfterSuite
     public void after() {
         if (driver != null) {
             driver.quit();
@@ -108,46 +94,13 @@ public class ParallelExecution extends BaseUtilities {
         }
     }
 
-@Test(threadPoolSize = 2, invocationCount = 2,  timeOut = 10000)
-public void parallelChromeTest() {
-
+@Test
+public void testParallelChromeTest() {
         driver.get(appURL);
         System.out.println("Test Case One with Thread Id:- "
                 + Thread.currentThread().getId());
-        orderMealSteps.user_is_on_take_away_landing_page();
-        orderMealSteps.user_can_see_time_to_order_food_message();
-        orderMealSteps.user_enter_search_for_address(getConfigPropertyValue(propertyFile, "address"));
-        orderMealSteps.search_results_popup_is_shown();
-        orderMealSteps.user_select_address_from_search_results(getConfigPropertyValue(propertyFile, "address"));
-        orderMealSteps.user_is_on_searched_address_page();
-        orderMealSteps.user_can_listed_restaurants_list();
-        orderMealSteps.user_select_restaurants_from_address_list();
-        orderMealSteps.user_is_on_restaurant_details_page(getConfigPropertyValue(propertyFile, "restaurant_name"));
-        orderMealSteps.user_select_first_menu_on_the_menu_list();
-        orderMealSteps.user_select_menu_drink_selected();
-        orderMealSteps.user_can_see_menu_total_price_to_pay();
-        orderMealSteps.user_select_button_to_add_menu_to_cart();
-        orderMealSteps.user_can_see_cart_total_price();
-        orderMealSteps.user_select_cart_order_button();
-        orderMealSteps.user_is_on_ready_to_eat_page();
-        orderMealSteps.user_can_see_delivery_address_details_header();
-        orderMealSteps.user_enter_delivery_address(getConfigPropertyValue(propertyFile, "delivery_address"));
-        orderMealSteps.user_enter_delivery_postal_code(getConfigPropertyValue(propertyFile, "postal_code"));
-        orderMealSteps.user_enter_delivery_city(getConfigPropertyValue(propertyFile, "city"));
-        orderMealSteps.user_enter_delivery_person_name(getConfigPropertyValue(propertyFile, "person_name"));
-        orderMealSteps.user_enter_email(getConfigPropertyValue(propertyFile, "email"));
-        orderMealSteps.user_enter_delivery_phone_number("phone_number");
-        orderMealSteps.user_enter_company_name("company_name");
-        orderMealSteps.user_select_delivery_time(getConfigPropertyValue(propertyFile, "delivery_time"));
-        orderMealSteps.user_enter_delivery_remarks(getConfigPropertyValue(propertyFile, "remarks"));
-        orderMealSteps.user_select_save_delivery_remarks_for_next_order();
-        orderMealSteps.user_select_pay_with_option("€ 19,00");
-        orderMealSteps.user_select_recieve_discounts_loyalty_and_updates();
-        orderMealSteps.user_select_order_and_pay_button();
-        orderMealSteps.user_can_see_thank_you_for_your_order_message();
-        orderMealSteps.user_can_see_copy_food_tracker_link();
-        orderMealSteps.user_can_see_restaurant_order_from_name(getConfigPropertyValue(propertyFile, "restaurant_name"));
-        orderMealSteps.user_can_see_ordered_meal_details();
-        orderMealSteps.user_can_see_payment_success_reference_number();
+            String landingPageTitle = driver.getTitle();
+            Assert.assertEquals(getConfigPropertyValue(propertyFile, "landing_page_title"), landingPageTitle);
+            secondsDelay(4);
     }
 }
